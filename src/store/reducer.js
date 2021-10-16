a81fd39856f1e40be35596812ef3dc6c1f2bb9bf
Ganesh <49611878+ganeshnikam29@users.noreply.github.com>
2021-10-16 17:18:05 +0530

    update

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,14 +1,37 @@
 const initialState = {
   counter: 0,
+  results: [],
 };
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case 'INCREMENT':
-      return { counter: state.counter + 1 };
+      return {
+        ...state,
+        counter: state.counter + 1,
+      };
     case 'DECREMENT':
-      return { counter: state.counter - 1 };
+      return {
+        ...state,
+        counter: state.counter - 1,
+      };
     case 'ADDFIVEVALUE':
-      return { counter: state.counter + 5 };
+      return {
+        ...state,
+        counter: state.counter + action.val,
+      };
+    case 'STORE_RESULT':
+      return {
+        ...state,
+        results: state.results.concat({ id: new Date(), val: state.counter }),
+      };
+    case 'DELETE_RESULT':
+      const updatedArray = state.results.filter(
+        (result) => result.id !== action.resultElId
+      );
+      return {
+        ...state,
+        results: updatedArray,
+      };
     default:
       return state;
   }
